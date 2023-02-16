adfd41e41a00b32760d6c9af0dce46ab4d94e685
gabriel <nykogabriel@gmail.com>
2023-02-16 16:01:19 -0300

    removed error simulating

--- tests/test_link.py
+++ tests/test_link.py
@@ -1,13 +1,9 @@
 from src.domain.entities import Link
 
+
 def test_should_save_new_valid_link(client):
     path = "/save"
-    data = {
-        "url": "teste.com"
-    }
-    
-    assert 1 == 2
-
+    data = {"url": "teste.com"}
     # response = client.post(path + f"?url={data['url']}")
     # assert response.status_code == 200
     # assert "key" in response.json().keys()
