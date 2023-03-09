58258cb43bae1c021f01782b9f375a6014ad1c69
Gabriel Verle <gabriel.verle@pti.org.br>
2023-03-09 16:40:03 -0300

    Added Prometheus support. /metrics

--- src/web/server.py
+++ src/web/server.py
@@ -1,14 +1,17 @@
 import os
 
 from fastapi import FastAPI
+from prometheus_fastapi_instrumentator import Instrumentator
 
 from src.web.routes import link_routes, misc_routes
+
 
 # Core Application Instance
 app = FastAPI(
     title="URL Shortener",
     version=os.getenv("PROJECT_VERSION", "Unknown version"),
 )
+instrumentator = Instrumentator().instrument(app).expose(app)
 
 # Add Routers
 app.include_router(misc_routes.router)
